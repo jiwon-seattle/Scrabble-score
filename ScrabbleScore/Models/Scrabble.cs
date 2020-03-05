c79e70676b2870a80387c2999012585ecf491b02
Andriy Veremyeyev <belyybrat@gmail.com>
2020-03-05 14:53:04 -0800

    Fix issues with methods, app provides values for every character of string and total value Co-authored-by: Jiwon Han  <jiwon1.han@gmail.com>

--- ScrabbleScore/Models/Scrabble.cs
+++ ScrabbleScore/Models/Scrabble.cs
@@ -1,12 +1,12 @@
 using System;
+using System.Linq;
 
 namespace Words.Models
 {
   public class Scrabble
   {  
     public string Word;
-    public char[] ArrayOfCharacters;
-    public char [] ArrayOfValues;
+    public int [] ArrayOfValues;
     public char [] ScrabbleOne;
     public char [] ScrabbleTwo;
     public char [] ScrabbleThree;
@@ -15,20 +15,11 @@ namespace Words.Models
     public char [] ScrabbleEight;
     public char [] ScrabbleTen;
 
-    
-    // public Dictionary<char[], int> OneScrabble;  
-    // public Dictionary<char[], int> TwoScrabble;
-    // public Dictionary<char[], int> ThreeScrabble;
-    // public Dictionary<char[], int> FourScrabble;
-    // public Dictionary<char[], int> FiveScrabble;
-    // public Dictionary<char[], int> EightScrabble;
-    // public Dictionary<char[], int> TenScrabble;
    
     public Scrabble (string word)   
     { 
       Word = word;
-      ArrayOfCharacters = word.ToCharArray(0, word.Length);
-      ArrayOfValues = new char [word.Length];
+      ArrayOfValues = new int [word.Length];
       ScrabbleOne = new char[] {'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'};
       ScrabbleTwo = new char[] {'d', 'g'};
       ScrabbleThree = new char [] {'b', 'c', 'm', 'p'};
@@ -36,74 +27,105 @@ namespace Words.Models
       ScrabbleFive = new char [] {'k'};
       ScrabbleEight = new char [] {'j', 'x'};
       ScrabbleTen = new char [] {'q', 'z'};
-      // OneScrabble.Add(ScrabbleOne, 1);
-      // TwoScrabble.Add(ScrabbleTwo, 2);
-      // ThreeScrabble.Add(ScrabbleThree, 3);
-      // FourScrabble.Add(ScrabbleFour, 4);
-      // FiveScrabble.Add(ScrabbleFour, 5);
-      // EightScrabble.Add(ScrabbleEight, 8);
-      // TenSrabble.Add(ScrabbleTen, 10);
-
     }
     
     
 
 
-    // public void CheckingScrabbleOne()
-    // {
-    //    for (int i = 0; i < ArrayOfCharacters.Length; i++)
-    //   {
-    //     if (ScrabbleOne.Contains('a')
-    //     {
-          
-    //     }
-    //   }
-    // }
+    public void CheckingScrabbleOne()
+    {
+       for (int i = 0; i < Word.Length; i++)
+      {
+        if (ScrabbleOne.Contains(Word[i]))
+        {
+          ArrayOfValues[i] = 1;
+        }
+      }
+    }
 
-    // public int SumOfValues()
-    // {
-      
-    // }
-    // public int CheckingCharacters(string input)
-    // {
+    public void CheckingScrabbleTwo()
+    {
+       for (int i = 0; i < Word.Length; i++)
+      {
+        if (ScrabbleTwo.Contains(Word[i]))
+        {
+          ArrayOfValues[i] = 2;
+        }
+      }
+    }
 
-    //   int index = 0;
-      
-    //     for (int i = 0; i < Input.Length; i++)
-    //     {
-    //       if (Input[i].Contains(ScrabbleOne[i]))
-    //         {
-    //           return 1;
-    //         }
-    //       else if (stringToArray[i].Contains(ScrabbleTwo))
-    //         {
-    //             return 2;
-    //         }
-    //       else if (stringToArray[i].Contains(ScrabbleThree))
-    //         {
-    //             return 3;
-    //         }
-    //       else if (stringToArray[i].Contains(ScrabbleFour))
-    //         {
-    //             return 4;
-    //         }
-    //       else if (reponseToArray[i].Contains(scrabbleFive))
-    //         {
-    //           return 5;
-    //         }
-    //       else if (reponseToArray[i].Contains(scrabbleEight))
-    //         {
-    //           return 8;
-    //         }
-    //       else if (responseToArray[i].Contains(scrabbleTen))
-    //         {
-    //           return 10;
-    //         }
-    //       else 
-    //         {
-    //           return 0;
-    //         }
-    //     } return index;
-    // }
+    public void CheckingScrabbleThree()
+    {
+       for (int i = 0; i < Word.Length; i++)
+      {
+        if (ScrabbleThree.Contains(Word[i]))
+        {
+          ArrayOfValues[i] = 3;
+        }
+      }
+    } 
+
+    public void CheckingScrabbleFour()
+    {
+       for (int i = 0; i < Word.Length; i++)
+      {
+        if (ScrabbleFour.Contains(Word[i]))
+        {
+          ArrayOfValues[i] = 4;
+        }
+      }
+    } 
+
+    public void CheckingScrabbleFive()
+    {
+       for (int i = 0; i < Word.Length; i++)
+      {
+        if (ScrabbleFive.Contains(Word[i]))
+        {
+          ArrayOfValues[i] = 5;
+        }
+      }
+    } 
+
+    public void CheckingScrabbleEight()
+    {
+       for (int i = 0; i < Word.Length; i++)
+      {
+        if (ScrabbleEight.Contains(Word[i]))
+        {
+          ArrayOfValues[i] = 8;
+        }
+      }
+    } 
+    public void CheckingScrabbleTen()
+    {
+       for (int i = 0; i < Word.Length; i++)
+      {
+        if (ScrabbleTen.Contains(Word[i]))
+        {
+          ArrayOfValues[i] = 10;
+        }
+      }
+    }
+
+    public void CheckingAllScrabbles()
+    {
+      CheckingScrabbleOne();
+      CheckingScrabbleTwo();
+      CheckingScrabbleThree();
+      CheckingScrabbleFour();
+      CheckingScrabbleFive();
+      CheckingScrabbleEight();
+      CheckingScrabbleTen();
+    }         
+    public int SummOfValues()
+    {
+      int index = 0;
+      for (int i=0; i<ArrayOfValues.Length; i++)
+      {
+        index +=ArrayOfValues[i];
+      }
+      return index;
+    }
   }
 }
--- ScrabbleScore/Program.cs
+++ ScrabbleScore/Program.cs
@@ -1,46 +1,25 @@
 using System;
 using System.Text.RegularExpressions;
 using System.Linq;
-// using Words.Models;
+using System.Collections.Generic;
+using Words.Models;
 
   class Program
   {
   static void Main()
   {
 
-    char [] arrayOne = {'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'};
-    List<string> result;
     Console.WriteLine("Please provide some word");
     string response = Console.ReadLine();
-    int index = 0;
-    for (int i=0; i<arrayOne.Length; i++)
+    Scrabble newScrabble = new Scrabble(response);
+    newScrabble.CheckingAllScrabbles();
+    Console.WriteLine("");
+    Console.WriteLine($"Scrabble Score for every letter in your word is:");
+    for (int i=0; i<response.Length; i++)
     {
-      if (arrayOne.Contains(response[i]))
-      {
-        R
-      }
-
+      Console.Write($"{newScrabble.ArrayOfValues[i]}, ");
     }
-    //Console.WriteLine($"You have {index} scrabble letters in your word");
-
-      // string pattern = @"\b[a]\w+";
-      // Regex rg = new Regex(pattern); 
-
-      // MatchCollection matchedResponse = rg.Matches(response); 
-
-      // // Console.WriteLine(newScrabble.Word);
-      // for (int i=0; i<responseNew.Length; i++)
-      // {
-      //   Console.WriteLine(responseNew[i]);
-      // }
-
-      // Console.WriteLine(responseNew[0]);
-      
-      // for (int count = 0; count < matchedreponse.Count; count++)  
-      //     {Console.WriteLine(matchedresponse[count].Value);  
-      //     }
+    Console.WriteLine("");
+    Console.WriteLine($"Total Scrabble Score of your word is {newScrabble.SummOfValues()}");
   }
-  
-}
-
-
+  }
